0c4714fa91c84aa4bc5ae97ce55295f954adcdfa
vivek rathod <103040999+vivek-rathod-prowerse@users.noreply.github.com>
2025-02-22 02:06:45 +0530

    db added

--- sitcom_ediction/settings.py
+++ sitcom_ediction/settings.py
@@ -84,11 +84,20 @@ WSGI_APPLICATION = 'sitcom_ediction.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+"""
+
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(default="postgresql://postgres:wCMuNetSwHMsyQTMsWUBflKmVbGFiabW@tramway.proxy.rlwy.net:45400/railway")
 }
 
 STATIC_URL = '/static/'
